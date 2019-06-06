d1f4fe6f16008de5719c1afc85f06357a287cde5
hritel <houssine04@gmail.com>
2019-06-06 17:17:25 +0200

    add some data by faker

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -5,25 +5,26 @@ namespace App\DataFixtures;
 use App\Entity\Article;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
 
 class ArticleFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
 
-        $i = 1;
-        while ($i <= 10) {
+        $faker = Factory::create('fr_FR');
+
+        for ($i = 0; $i < 10; $i++) {
             $article = new Article();
-            $article->setTitle("titre test " . $i)
-                ->setContent("text text text")
-                ->setDate(new \DateTime());
+            $article->setTitle($faker->text(50))
+                ->setContent($faker->text)
+                ->setDate($faker->dateTimeBetween('-1 years', 'now'));
 
             $manager->persist($article);
-            $i++;
+
         }
 
         $manager->flush();
-
 
     }
 }
